444969bb482f0c6ab0b2bae268d319cb842ee8de
DAM1P <DAM1P@DAMPC2>
2025-03-31 13:39:17 +0200

    Revert "Cambio rama modificarPersona"
    This reverts commit 1cd40f4ddfb1b6075c50876b96d9417edcfa2867.

--- src/main/java/com/mycompany/ejemplogithub/Persona.java
+++ src/main/java/com/mycompany/ejemplogithub/Persona.java
@@ -14,9 +14,8 @@ public class Persona {
     private int edad;
 
     // Constructor
-    //Los nombres en mayúsculas
     public Persona(String nombre, int edad) {
-        this.nombre.toUpperCase();
+        this.nombre = nombre;
         this.edad = edad;
     }
 
